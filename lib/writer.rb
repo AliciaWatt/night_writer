d991cd5495089d307dc6820fec2000ba59239e2c
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-07 15:45:18 -0700

    feat: write method and two_dots methods

--- lib/writer.rb
+++ lib/writer.rb
@@ -31,4 +31,54 @@ class Writer
       text_to_braille_hash[letter]
     end
   end
+
+  def write
+    letters = translator
+    until letters == []
+      first_elements = letters.shift(20)
+      first_two_dots(first_elements)
+      middle_two_dots(first_elements)
+      last_two_dots(first_elements)
+    end
+    letters
+  end
+
+  def first_two_dots(elements)
+    File.open(translate_file, 'a') do |file|
+      elements.each do |array|
+        if array.length == 6
+          file.write "#{array[0]}#{array[1]}"
+        else
+          file.write "#{array[0]}#{array[1]}#{array[2]}#{array[3]}"
+        end
+      end
+      file.write "\n"
+    end
+  end
+
+  def middle_two_dots(elements)
+    File.open(translate_file, 'a') do |file|
+      elements.each do |array|
+        if array.length == 6
+          file.write "#{array[2]}#{array[3]}"
+        else
+          file.write "#{array[4]}#{array[5]}#{array[6]}#{array[7]}"
+        end
+      end
+      file.write "\n"
+    end
+  end
+
+  def last_two_dots(elements)
+    File.open(translate_file, 'a') do |file|
+      elements.each do |array|
+        if array.length == 6
+          file.write "#{array[4]}#{array[5]}"
+        else
+          file.write "#{array[8]}#{array[9]}#{array[10]}#{array[11]}"
+        end
+      end
+      file.write "\n"
+    end
+  end
 end
