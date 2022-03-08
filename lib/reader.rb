bb4bae993f4e5c2bcdfbca4e1c2db397afcbdd65
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-07 21:37:42 -0700

    feat: braille_by_lines method

--- lib/reader.rb
+++ lib/reader.rb
@@ -8,4 +8,29 @@ class Reader
     @translate_file = files[1]
     @converter = Convert.new
   end
+
+  def split_braille
+    braille_by_char = []
+    braille_by_lines.each do |line|
+      split_line = line.split('')
+      num = 0
+      until (split_line == [])
+        braille_by_char[num.floor] ||= []
+        braille_by_char[num.floor] << split_line.shift
+        num += 0.5
+      end
+    end
+    braille_by_char
+  end
+
+  def braille_by_lines
+    braille_by_lines = ['', '', '']
+    split_braille = File.readlines(braille_file, chomp: true)
+    until split_braille == []
+      braille_by_lines[0] << split_braille.shift
+      braille_by_lines[1] << split_braille.shift
+      braille_by_lines[2] << split_braille.shift
+    end
+    braille_by_lines
+  end
 end
