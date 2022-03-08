4c2b4c13104a02cb6ca6e8ceead93dd9fcf6f91d
Alicia Watt <81201783+AliciaWatt@users.noreply.github.com>
2022-03-08 16:13:45 -0700

    Revert "Iteration 3"

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,12 +1,7 @@
 require_relative 'writer'
-class NightWriter
-  def self.write
-    text_file_names = ARGV
-    # ARGV captures command line arguments in an array
-    writer = Writer.new(text_file_names)
-    writer.creation_message
-    writer.write if File.file?(text_file_names[0])
-  end
-end
 
-NightWriter.write
+text_file_names = ARGV
+# ARGV captures command line arguments in an array
+
+writer = Writer.new(text_file_names)
+writer.creation_message
